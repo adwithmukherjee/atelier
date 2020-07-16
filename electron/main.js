9aaf2c45192312890523a6d29790224b3e0f9947
adwithmukherjee <adwithmukherjee@gmail.com>
2020-07-15 22:45:18 -0400

    command j functionality

--- electron/main.js
+++ electron/main.js
@@ -1,23 +1,49 @@
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+const { create } = require("domain");
 
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');   
-const path = require('path');
- 
 let mainWindow;
- 
+
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width:800,
-        height:600,
-        show: false
-    });
-    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
- 
-    mainWindow.loadURL(startURL);
- 
-    mainWindow.once('ready-to-show', () => mainWindow.show());
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+  });
+  const startURL = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once("ready-to-show", () => mainWindow.show());
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
-app.on('ready', createWindow);
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.whenReady().then(() => {
+  globalShortcut.register("CommandOrControl+J", () => {
+    console.log("CommandJ is pressed");
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    } else {
+        BrowserWindow.getAllWindows()[0].show()
+    }
+  });
+});
