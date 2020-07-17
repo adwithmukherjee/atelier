f29fb8938b67ec75aa2912414619d7771dd15d6b
josh_ip <joshua.ip@yale.edu>
2020-07-17 01:59:56 -0500

    fiddling

--- electron/main.js
+++ electron/main.js
@@ -1,27 +1,44 @@
-const { app, BrowserWindow, globalShortcut } = require("electron");
+const { app, BrowserWindow, globalShortcut, Menu } = require("electron");
 const isDev = require("electron-is-dev");
 const path = require("path");
 const { create } = require("domain");
 
 let mainWindow;
-
+Menu.setApplicationMenu(null);
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     show: false,
+    frame: false,
+    movable: true,
   });
   const startURL = isDev
     ? "http://localhost:3000"
     : `file://${path.join(__dirname, "../build/index.html")}`;
 
   mainWindow.loadURL(startURL);
-
+  //app.dock.hide();
+  //mainWindow.setAlwaysOnTop(true, "floating");
+  //mainWindow.setVisibleOnAllWorkspaces(true);
+  //mainWindow.setFullScreenable(true);
+  //mainWindow.setFullScreenable(false);
+  //mainWindow.setFullScreenable(false);
+  app.dock.hide();
   mainWindow.once("ready-to-show", () => mainWindow.show());
-
+  mainWindow.resizable = false;
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
+  mainWindow.on("swipe", console.log);
+  mainWindow.on("rotate-gesture", (e) => {
+    console.log("rotate-gesture");
+  });
+  // mainWindow.on("swipe", () => {
+  //   mainWindow.setFullScreenable(true);
+  //   console.log("swiped");
+  //   //console.log(e);
+  // });
 }
 app.on("ready", createWindow);
 
@@ -38,12 +55,33 @@ app.on("activate", () => {
 });
 
 app.whenReady().then(() => {
+  console.log(mainWindow.isMovable());
   globalShortcut.register("CommandOrControl+J", () => {
     console.log("CommandJ is pressed");
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
+      createWindow();
     } else {
-        BrowserWindow.getAllWindows()[0].show()
+      // app.dock.hide();
+      // mainWindow.setAlwaysOnTop(true, "floating");
+      // mainWindow.setVisibleOnAllWorkspaces(true);
+
+      app.dock.hide();
+      mainWindow.setAlwaysOnTop(true, "floating");
+      mainWindow.setVisibleOnAllWorkspaces(true);
+      //mainWindow.setFullScreenable(true);
+      mainWindow.setFullScreenable(false);
+      //BrowserWindow.getAllWindows()[0].show();
+    }
+  });
+  globalShortcut.register("Esc", () => {
+    //app.dock.show();
+
+    //close window with esc
+    console.log("escaped");
+    if (BrowserWindow.getAllWindows().length === 0) {
+      //createWindow();
+    } else {
+      mainWindow.close();
     }
   });
 });
--- src/components/App.js
+++ src/components/App.js
@@ -3,42 +3,49 @@
 //if the class exports a component, upper case the first letter
 import React, { Component } from "react";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
-import Header from "./Header"; 
-import { connect } from 'react-redux'; 
-import * as actions from '../actions' 
+import Header from "./Header";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
+const Dashboard = () => (
+  <h2 style={{ "-webkit-app-region": "drag" }}>
+    Dashboard - here go the notes
+  </h2>
+);
+const Landing = () => (
+  <div style={{ textAlign: "center" }}>
+    <h1> Welcome to Atelier! </h1>
+    <h3>please login</h3>
+  </div>
+);
 
-const Dashboard = () => <h2> Dashboard - here go the notes</h2>;
-const Landing = () => <div style = {{textAlign: "center"}}><h1 > Welcome to Atelier! </h1><h3>please login</h3></div>;
-
-class App extends Component{
-  //we use class-based so we can access these lifetime cycle methods 
-  //it is here we make our initial ajax requests. 
+class App extends Component {
+  //we use class-based so we can access these lifetime cycle methods
+  //it is here we make our initial ajax requests.
   componentDidMount() {
-    this.props.fetchUser(); 
- 
+    this.props.fetchUser();
   }
-
-  
 
   render() {
     return (
-    <div className = "container">
-     
-      <BrowserRouter>
-        <div>
-          
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          
+      <body style={{ "-webkit-app-region": "drag" }}>
+        <div className="container" style={{ "-webkit-app-region": "drag" }}>
+          <BrowserRouter>
+            <div>
+              <Header />
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/dashboard" component={Dashboard} />
+            </div>
+          </BrowserRouter>
         </div>
-      </BrowserRouter>
-    </div>
+      </body>
     );
   }
-};
+}
 
-//connect is used to connect REact and Redux. 
+//connect is used to connect REact and Redux.
 //basically adds actions to the props of App class
 export default connect(null, actions)(App);
+
+//https://stackoverflow.com/questions/39769513/react-form-to-submit-object-which-is-then-pushed-to-array
+//https://reactjs.org/docs/forms.html
