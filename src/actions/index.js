5b2cc1cdd124540cacaa10e0febbebd1bcddfcec
adwithmukherjee <adwithmukherjee@gmail.com>
2020-07-18 14:56:20 -0400

    task wiring redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { FETCH_USER, FETCH_SURVEYS } from "./types";
+import { FETCH_USER, FETCH_TASKS } from "./types";
 
 
-export const fetchUser = () => 
+export const fetchUser = () => async (dispatch) => {
   //making a get request to our backend, to the /api/current_user route
 
   //when the fetchUser action creator returns a function, Redux Thunk automatically
@@ -10,17 +10,21 @@ export const fetchUser = () =>
   //remember, dispatch is one catch-all function that takes in an Action object and sends to reducers. 
   //this allows us to wait until the get function is asynchronously completed
   
-  
-  async (dispatch) => {
-    
     const res = await axios.get("/api/current_user")
     dispatch({type: FETCH_USER, payload: res.data })
     //dispatch an action with payload of the api's response
 
-  };
+};
 
 
+export const fetchTasks = () => async (dispatch) => {
+  const res = await axios.get("/tasks")
+  dispatch({type: FETCH_TASKS, payload: res.data})
+}
 
+export const submitTask = (values) => async (dispatch) => {
+  await axios.post("/tasks", values)
+}
 
 //we're only putting the relative path to
 //our backend here.
